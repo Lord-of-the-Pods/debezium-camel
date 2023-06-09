2a8fbd585d3e51063aac533ee02bfcb13fab2226
Abhishek Vishnoi <abvishno@redhat.com>
2023-06-09 13:27:10 +0530

    first commit

--- src/main/java/com/example/sbcamelkafkaproducer/MyRoute.java
+++ src/main/java/com/example/sbcamelkafkaproducer/MyRoute.java
@@ -51,6 +51,15 @@ public class MyRoute extends RouteBuilder {
                 .log("    with the offset ${headers[kafka.OFFSET]}")
                 .log("    with the key ${headers[kafka.KEY]}");
 
+
+        from("kafka:{{topic.debezium}}?brokers={{broker}}")
+                .log("Message received from Kafka : ${body}")
+                .log("    on the topic ${headers[kafka.TOPIC]}")
+                .log("    on the partition ${headers[kafka.PARTITION]}")
+                .log("    with the offset ${headers[kafka.OFFSET]}")
+                .log("    with the key ${headers[kafka.KEY]}");
+
+
     }
 
 }
